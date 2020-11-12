0c7a52a44689320131198fe7475e20594271cfc6
IanRdz <girm.00@hotmail.com>
2020-11-12 12:19:06 -0600

    Cambio En index, estilos y JS

---
+++ Proyecto/js/funciones.js
@@ -0,0 +1,28 @@
+function cambiarFormLF() {
+    var rf = document.getElementById("rf");
+    var lf = document.getElementById("lf")
+    var cf = document.getElementById("cf");
+    cf.style.display = "none";
+    rf.style.display = "none";
+    lf.style.display = "block";
+  }
+  
+  function cambiarFormRF() {
+    var rf = document.getElementById("rf");
+    var lf = document.getElementById("lf");
+    var cf = document.getElementById("cf");
+    cf.style.display = "none";
+    lf.style.display = "none";
+    rf.style.display = "block";
+  }
+  
+  function cambiarFormCF() {
+    var rf = document.getElementById("rf");
+    var lf = document.getElementById("lf")
+    var cf = document.getElementById("cf");
+    cf.style.display = "block";
+    rf.style.display = "none";
+    lf.style.display = "none";
+  }
+  
+  
