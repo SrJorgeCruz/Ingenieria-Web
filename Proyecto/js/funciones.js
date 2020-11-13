ff9b33ae326207a568fc5a43c5439937466a00fa
IanRdz <girm.00@hotmail.com>
2020-11-12 22:52:23 -0600

    Parte uno completa

--- Proyecto/js/funciones.js
+++ Proyecto/js/funciones.js
@@ -1,27 +1,68 @@
 function cambiarFormLF() {
   var rf = document.getElementById("rf");
-  var lf = document.getElementById("lf")
+  var lf = document.getElementById("lf");
   var cf = document.getElementById("cf");
+  var df = document.getElementById("df");
+  var ef = document.getElementById("ef");
   cf.style.display = "none";
   rf.style.display = "none";
   lf.style.display = "block";
+  df.style.display ="none";
 }
 
 function cambiarFormRF() {
   var rf = document.getElementById("rf");
   var lf = document.getElementById("lf");
   var cf = document.getElementById("cf");
+  var df = document.getElementById("df");
+  var ef = document.getElementById("ef");
   cf.style.display = "none";
   lf.style.display = "none";
   rf.style.display = "block";
+  df.style.display =" none";
+  ef.style.display = "none";
 }
 
 function cambiarFormCF() {
   var rf = document.getElementById("rf");
-  var lf = document.getElementById("lf")
+  var lf = document.getElementById("lf");
   var cf = document.getElementById("cf");
+  var df = document.getElementById("df");
+  var ef = document.getElementById("ef");
   cf.style.display = "block";
   rf.style.display = "none";
   lf.style.display = "none";
+  df.style.display =" none";
+  ef.style.display = "none";
 }
+
+function cambiarFormDF() {
+//console.log("Hola");
+
+  var rf = document.getElementById("rf");
+  var lf = document.getElementById("lf");
+  var cf = document.getElementById("cf");
+  var df = document.getElementById("df");
+  var ef = document.getElementById("ef");
+  cf.style.display = "none";
+  rf.style.display = "none";
+  lf.style.display = "none";
+  df.style.display = "block";
+  ef.style.display = "none";
+
+}
+  function cambiarFormEF(){
+
+    var rf = document.getElementById("rf");
+    var lf = document.getElementById("lf");
+    var cf = document.getElementById("cf");
+    var df = document.getElementById("df");
+    var ef = document.getElementById("ef");
+    cf.style.display = "none";
+    rf.style.display = "none";
+    lf.style.display = "none";
+    df.style.display = "none";
+    ef.style.display = "block";
+
+  }
 
