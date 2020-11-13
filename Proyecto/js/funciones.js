1468f6294f4690a02cd93ff76dca25b7a2fe4ec7
Daniel Gonzalez <enda0507@gmail.com>
2020-11-12 19:52:59 -0600

    Se modifico el archivo index, el EstilosGenerales al igual se va a agregar un nuevo archivo para la pagina alumnos.

--- Proyecto/js/funciones.js
+++ Proyecto/js/funciones.js
@@ -1,28 +1,27 @@
 function cambiarFormLF() {
-    var rf = document.getElementById("rf");
-    var lf = document.getElementById("lf")
-    var cf = document.getElementById("cf");
-    cf.style.display = "none";
-    rf.style.display = "none";
-    lf.style.display = "block";
-  }
-  
-  function cambiarFormRF() {
-    var rf = document.getElementById("rf");
-    var lf = document.getElementById("lf");
-    var cf = document.getElementById("cf");
-    cf.style.display = "none";
-    lf.style.display = "none";
-    rf.style.display = "block";
-  }
-  
-  function cambiarFormCF() {
-    var rf = document.getElementById("rf");
-    var lf = document.getElementById("lf")
-    var cf = document.getElementById("cf");
-    cf.style.display = "block";
-    rf.style.display = "none";
-    lf.style.display = "none";
-  }
-  
-  
+  var rf = document.getElementById("rf");
+  var lf = document.getElementById("lf")
+  var cf = document.getElementById("cf");
+  cf.style.display = "none";
+  rf.style.display = "none";
+  lf.style.display = "block";
+}
+
+function cambiarFormRF() {
+  var rf = document.getElementById("rf");
+  var lf = document.getElementById("lf");
+  var cf = document.getElementById("cf");
+  cf.style.display = "none";
+  lf.style.display = "none";
+  rf.style.display = "block";
+}
+
+function cambiarFormCF() {
+  var rf = document.getElementById("rf");
+  var lf = document.getElementById("lf")
+  var cf = document.getElementById("cf");
+  cf.style.display = "block";
+  rf.style.display = "none";
+  lf.style.display = "none";
+}
+
