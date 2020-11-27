c178d6326153e451a4e7baf68072486a288a0f6c
SrJorgeCruz <jorge_15243@hotmail.com>
2020-11-26 19:52:41 -0600

    Agregamos un index para cada form, intentamos leer archivo JSON y ponerlo en las cajas de texto

--- Proyecto/src/routes/index.js
+++ Proyecto/src/routes/index.js
@@ -22,10 +22,41 @@ router.get('/paginaCoordinador',(req, res)=>{
     res.render('PaginaCoordinador',{lista});
 });
 
+
 router.get('/nuevosProductos',(req, res)=>{
-    res.render('index');
+    res.render('index0');
 });
 
+router.get('/index1/:ACongreso.titulo',(req, res)=>{
+  let tit = req.params.titulo;
+  let indice=0;
+  let contador=0;
+  let producto;  
+  productos.ACongreso.forEach(element => {
+    if(element.productos.ACongreso.titulo == tit)
+    { 
+      indice=contador;
+    }
+      contador ++;
+    })
+    producto=productos.ACongreso[indice];
+  res.render('index1');
+});
+
+router.get('/index2',(req, res)=>{
+  res.render('index2');
+});
+
+router.get('/index3',(req, res)=>{
+  res.render('index3');
+});
+
+router.get('/index4',(req, res)=>{
+  res.render('index4');
+});
+router.get('/index5',(req, res)=>{
+  res.render('index5');
+});
 
 
 module.exports = router;
