6b38fb97d827ea3d1f18ed2ead77fd9e698e5b7b
IanRdz <girm.00@hotmail.com>
2020-11-26 21:28:58 -0600

    Consulta para poder modificar, faltan select

--- Proyecto/src/routes/index.js
+++ Proyecto/src/routes/index.js
@@ -49,19 +49,73 @@ router.get('/index1/:titulo',(req, res)=>{
   res.render('index1',{producto});
 });
 
-router.get('/index2',(req, res)=>{
-  res.render('index2');
+
+router.get('/index2/:titulo',(req, res)=>{
+  let tit = req.params.titulo;
+  let indice=0;
+  let contador=0;
+  let producto;  
+  productos.ARevista.forEach(element => {
+    if(element.titulo == tit)
+    { 
+      indice=contador;
+    }
+      contador ++;
+    })
+  producto=productos.ARevista[indice];
+  res.render('index2',{producto});
 });
 
-router.get('/index3',(req, res)=>{
-  res.render('index3');
+
+router.get('/index3/:titulo',(req, res)=>{
+  let tit = req.params.titulo;
+  let indice=0;
+  let contador=0;
+  let producto;  
+  productos.CLibro.forEach(element => {
+    if(element.titulo == tit)
+    { 
+      indice=contador;
+    }
+      contador ++;
+    })
+  producto=productos.CLibro[indice];
+  res.render('index3',{producto});
 });
 
-router.get('/index4',(req, res)=>{
-  res.render('index4');
+
+router.get('/index4/:titulo',(req, res)=>{
+  let tit = req.params.titulo;
+  let indice=0;
+  let contador=0;
+  let producto;  
+  productos.libro.forEach(element => {
+    if(element.titulo == tit)
+    { 
+      indice=contador;
+    }
+      contador ++;
+    })
+  producto=productos.libro[indice];
+  res.render('index4',{producto});
 });
-router.get('/index5',(req, res)=>{
-  res.render('index5');
+
+
+
+router.get('/index5/:titulo',(req, res)=>{
+  let tit = req.params.titulo;
+  let indice=0;
+  let contador=0;
+  let producto;  
+  productos.hardwareSoftware.forEach(element => {
+    if(element.titulo == tit)
+    { 
+      indice=contador;
+    }
+      contador ++;
+    })
+  producto=productos.hardwareSoftware[indice];
+  res.render('index5',{producto});
 });
 
 
